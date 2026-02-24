78e48f5d32f28008a3b3ecf704300c817af14848
iacovone10-cmd <iacovone10@gmail.com>
2026-02-24 08:48:04 +0100

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const openBtn = document.getElementById('open');
-  const details = document.getElementById('details');
-
-  function open(){
-    details.scrollIntoView({behavior:'smooth'});
-  }
-  openBtn?.addEventListener('click', open);
-})();
