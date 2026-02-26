6fd188115a2e79f956cabe51fa60d957b92cbcc0
iacovone10-cmd <iacovone10@gmail.com>
2026-02-26 11:23:07 +0100

    Delete app.js

--- app.js
+++
@@ -1,1 +0,0 @@
-(()=>{const o=document.getElementById('open');const c=document.getElementById('close');const ov=document.getElementById('overlay');o.onclick=()=>{document.body.classList.add('open')};c.onclick=()=>{document.body.classList.remove('open')};})();
