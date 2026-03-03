b6f35f0c21047873a868e14b6ccfb13b1d90c74d
iacovone10-cmd <iacovone10@gmail.com>
2026-03-03 08:28:22 +0100

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-(()=>{
-  const openBtn=document.getElementById('open');
-  const closeBtn=document.getElementById('close');
-  const overlay=document.getElementById('overlay');
-  const pager=document.getElementById('pager');
-  openBtn.onclick=()=>{document.body.classList.add('open');pager.scrollTop=0;overlay.setAttribute('aria-hidden','false')}
-  closeBtn.onclick=()=>{document.body.classList.remove('open');overlay.setAttribute('aria-hidden','true')}
-})();
